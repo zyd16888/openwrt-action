9603086d263364e4ca6f4ae149064ad41260b638
zyd16888 <zyd475182659@163.com>
2021-04-16 13:37:41 +0800

    fix packages

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,10 +16,11 @@
 # Add a feed source
 # 第三方插件
 sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-sed -i '$a src-git OpenWrt-packages https://github.com/OpenWrt-Actions/OpenWrt-Packages' feeds.conf.default
+# sed -i '$a src-git OpenWrt-packages https://github.com/OpenWrt-Actions/OpenWrt-Packages' feeds.conf.default
 # git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
 # git clone https://github.com/OpenWrt-Actions/helloworld.git package/helloworld
 git clone https://github.com/xiaorouji/openwrt-passwall.git package/passwall
+git clone https://github.com/OpenWrt-Actions/OpenWrt-Packages.git package/OpenWrt-Packages
 # git clone https://github.com/maxlicheng/luci-app-unblockmusic.git package/luci-app-unblockmusic
 # git clone https://github.com/pymumu/smartdns.git package/smartdns
 # git clone https://github.com/pymumu/luci-app-smartdns.git package/luci-app-smartdns
