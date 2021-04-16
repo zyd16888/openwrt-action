7aefff17190286ec5b11c0add8cd585068315710
zyd16888 <zyd475182659@163.com>
2021-04-16 10:57:13 +0800

    Update diy-part1.sh
    添加插件

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,4 +14,17 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-#sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+# 第三方插件
+sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
+git clone https://github.com/OpenWrt-Actions/helloworld.git package/helloworld
+git clone https://github.com/xiaorouji/openwrt-passwall.git package/passwall
+git clone https://github.com/maxlicheng/luci-app-unblockmusic.git package/luci-app-unblockmusic
+git clone https://github.com/pymumu/smartdns.git package/smartdns
+git clone https://github.com/pymumu/luci-app-smartdns.git package/luci-app-smartdns
+
+# 主题
+sed -i '$a src-git infinityfreedom https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom.git' feeds.conf.default
+git clone https://github.com/Leo-Jo-My/luci-theme-opentomcat.git package/lean/luci-theme-opentomcat
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
+
