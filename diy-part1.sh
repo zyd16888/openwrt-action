8d259bfade02a3f4700a2f9a316e57f88ce0bfe0
zyd16888 <zyd475182659@163.com>
2021-04-16 13:02:17 +0800

    add OpenWrt-Packages

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,13 +16,14 @@
 # Add a feed source
 # 第三方插件
 sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
-git clone https://github.com/OpenWrt-Actions/helloworld.git package/helloworld
+sed -i '$src-git OpenWrt-packages https://github.com/OpenWrt-Actions/OpenWrt-Packages' feeds.conf.default
+# git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
+# git clone https://github.com/OpenWrt-Actions/helloworld.git package/helloworld
 git clone https://github.com/xiaorouji/openwrt-passwall.git package/passwall
-git clone https://github.com/maxlicheng/luci-app-unblockmusic.git package/luci-app-unblockmusic
-git clone https://github.com/pymumu/smartdns.git package/smartdns
-git clone https://github.com/pymumu/luci-app-smartdns.git package/luci-app-smartdns
+# git clone https://github.com/maxlicheng/luci-app-unblockmusic.git package/luci-app-unblockmusic
+# git clone https://github.com/pymumu/smartdns.git package/smartdns
+# git clone https://github.com/pymumu/luci-app-smartdns.git package/luci-app-smartdns
 
 # 主题
-sed -i '$a src-git infinityfreedom https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom.git' feeds.conf.default
-git clone https://github.com/Leo-Jo-My/luci-theme-opentomcat.git package/lean/luci-theme-opentomcat
+# sed -i '$a src-git infinityfreedom https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom.git' feeds.conf.default
+# git clone https://github.com/Leo-Jo-My/luci-theme-opentomcat.git package/lean/luci-theme-opentomcat
