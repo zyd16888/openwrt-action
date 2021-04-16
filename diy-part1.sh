3887ddcc1e9380b786df3385537a02130d0530b4
zyd16888 <zyd475182659@163.com>
2021-04-16 13:24:46 +0800

    add a, ...

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,7 +16,7 @@
 # Add a feed source
 # 第三方插件
 sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-sed -i '$src-git OpenWrt-packages https://github.com/OpenWrt-Actions/OpenWrt-Packages' feeds.conf.default
+sed -i '$a src-git OpenWrt-packages https://github.com/OpenWrt-Actions/OpenWrt-Packages' feeds.conf.default
 # git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
 # git clone https://github.com/OpenWrt-Actions/helloworld.git package/helloworld
 git clone https://github.com/xiaorouji/openwrt-passwall.git package/passwall
@@ -27,3 +27,15 @@ git clone https://github.com/xiaorouji/openwrt-passwall.git package/passwall
 # 主题
 # sed -i '$a src-git infinityfreedom https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom.git' feeds.conf.default
 # git clone https://github.com/Leo-Jo-My/luci-theme-opentomcat.git package/lean/luci-theme-opentomcat
+
+# src-git packages https://github.com/coolsnowwolf/packages
+# src-git luci https://github.com/coolsnowwolf/luci
+# src-git routing https://git.openwrt.org/feed/routing.git
+# src-git telephony https://git.openwrt.org/feed/telephony.git
+# #src-git video https://github.com/openwrt/video.git
+# #src-git targets https://github.com/openwrt/targets.git
+# #src-git management https://github.com/openwrt-management/packages.git
+# #src-git oldpackages http://git.openwrt.org/packages.git
+# #src-link custom /usr/src/openwrt/custom-feed
+# #src-git helloworld https://github.com/fw876/helloworld
+# src-git lienol https://github.com/Lienol/openwrt-package
