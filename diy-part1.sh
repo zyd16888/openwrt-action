a5686f482dca3fad8fac99b77df53dc4e98948ec
zyd16888 <zyd475182659@163.com>
2021-08-07 18:43:57 +0800

    update kenzok8

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,7 +16,9 @@
 # Add a feed source
 # 第三方插件
 sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-sed -i '$a src-git liuran001_packages https://github.com/liuran001/openwrt-packages' feeds.conf.default
+# sed -i '$a src-git liuran001_packages https://github.com/liuran001/openwrt-packages' feeds.conf.default
+sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 # sed -i '$a src-git OpenWrt-packages https://github.com/OpenWrt-Actions/OpenWrt-Packages' feeds.conf.default
 # git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
 # git clone https://github.com/OpenWrt-Actions/helloworld.git package/helloworld
